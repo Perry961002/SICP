212db1ee207ee2c1c3a21e3ec4bca66e7bb0c536
Perry961002 <Perry961002@163.com>
2018-11-23 18:34:06 +0800

    :lollipop:k项有限连分式过程

---
+++ Chap1/exercise/exe1.37-cont-frac.scm
@@ -0,0 +1,26 @@
+;k项有限连分式过程
+(define (cont-frac N D k)
+    (define (fun i)
+        (if (= i k)
+            (/ (N k) (D k))
+            (/ (N k) 
+               (+ (D k)
+                  (fun (+ i 1))))))
+    (fun 1))
+
+;选择k有项连分式过程的实现方式计算黄金分割率, k = 13时保证近似值具有0.0001的精度
+(define (golden-ratio kind-of-fun k)
+    (/ 1.0 
+       (kind-of-fun (lambda (i) 1.0)
+                  (lambda (i) 1.0)
+                  k)))
+;----------------------------------------------------------------
+;给出一个迭代版本的实现
+(define (cont-frac-iter N D k)
+    (define (iter i result)
+        (if (= i 1)
+            result
+            (iter (- i 1) (/ (N (- i 1))
+                             (+ (N (- i 1))
+                                result)))))
+    (iter k (/ (N k) (D k))))
