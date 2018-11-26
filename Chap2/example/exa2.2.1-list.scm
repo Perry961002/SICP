3498dae01707027fbf6e8776e82bc7874aa95fbe
Perry961002 <Perry961002@163.com>
2018-11-26 21:43:35 +0800

    :cookie:例2.2.1序列的表示

---
+++ Chap2/example/exa2.2.1-list.scm
@@ -0,0 +1,23 @@
+(define one-through-four (list 1 2 3 4))
+(car one-through-four)
+(cdr one-through-four)
+(cons 0 one-through-four)
+(cons one-through-four 5)
+;----------------------------------------
+;返回表的第n个项, scheme已经定义这个过程
+(define (list-ref items n)
+    (cond ((null? items) (display "error: list is null"))
+          ((= n 0) (car items))
+          (else (list-ref (cdr items) (- n 1)))))
+
+;返回表的长度, scheme已经定义
+(define (length items)
+    (if (null? items)
+        0
+        (+ 1 (length (cdr items)))))
+
+;合并两个表, scheme已经定义
+(define (append list1 list2)
+    (if (null? list1)
+        list2
+        (cons (car list1) (append (cdr list1) list2))))
