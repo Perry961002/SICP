13a009567393c163d42f70428a51e102b0128455
Perry961002 <Perry961002@163.com>
2018-11-26 19:35:22 +0800

    :cookie:习题2.10对除法的修改

---
+++ Chap2/exercise/exe2.10-div-better.scm
@@ -0,0 +1,7 @@
+(load "Chap2\\example\\exa2.1.4-Interval-arithmetic.scm")
+(define (div-interval x y)
+    (if (> 0 (* (lower-bound y) (upper-bound y)))
+        (error "error" (lower-bound y))
+        (mul-interval x
+                      (make-interval (/ 1.0 (upper-bound y))
+                                     (/ 1.0 (lower-bound y))))))
