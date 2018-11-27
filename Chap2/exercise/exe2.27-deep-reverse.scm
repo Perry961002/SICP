18ce26b474da7d3c11a383dd4d7980adea595fe4
Perry961002 <Perry961002@163.com>
2018-11-27 13:46:53 +0800

    :lollipop:习题2.27 deep-reverse

---
+++ Chap2/exercise/exe2.27-deep-reverse.scm
@@ -0,0 +1,5 @@
+(define (deep-reverse items)
+    (cond ((null? items) '())
+          ((not (pair? items)) items)
+          (else (append (deep-reverse (cdr items))
+                        (list (deep-reverse (car items)))))))
