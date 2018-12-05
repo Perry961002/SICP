793a769402a3e8ad0c41797725f7802f2f8aaf0c
Perry961002 <Perry961002@163.com>
2018-12-05 16:21:15 +0800

    :tada:例2.3.2 代数表达式的表示

--- Chap2/example/exa2.3.2-Symbolic-guidance.scm
+++ Chap2/example/exa2.3.2-Symbolic-guidance.scm
@@ -26,6 +26,60 @@
                 (make-product (multipliter exp)
                               (deriv (multiplicand exp) var))
                 (make-product (deriv (multipliter exp) var)
-                              (multiplicandexp))))
+                              (multiplicand exp))))
           (else
             (error "unknown expression type -- DERIV" exp))))
+;----------------------------------------------------------------------------
+;代数表达式的表示
+
+;变量就是符号，可以用基本谓词symbol?判断
+(define (variable? x) (symbol? x))
+
+;两个变量相同就是表示他们的符号相互eq?
+(define (same-variable? v1 v2)
+    (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+;和式和乘式都构造成表
+(define (make-sum a1 a2) (list '+ a1 a2))
+
+(define (make-product m1 m2) (list '* m1 m2))
+
+;和式就是第一个元素为符号+的表
+(define (sum? x)
+    (and (pair? x) (eq? (car x) '+)))
+
+;被加数是表示和式的表里的第二个元素
+(define (addend s) (cadr s))
+
+;加数是表示和式的表里的第三个元素
+(define (augend s) (caddr s))
+
+;乘式就是第一个元素为符号*的表
+(define (product? x)
+    (and (pair? x) (eq? (car x) '*)))
+
+;被乘数是表示乘式的表里的第二个元素
+(define (multipliter p) (cadr p))
+
+;乘数是表示乘式的表里的第三个元素
+(define (multiplicand p) (caddr p))
+
+;化简构造和式和乘式过程
+;定义=number?，检查某个表达式是否等于一个给定的数
+(define (=number? exp num)
+    (and (number? exp) (= exp num)))
+
+;化简和式
+(define (make-sum a1 a2)
+    (cond ((=number? a1 0) a2)
+          ((=number? a2 0) a1)
+          ((and (number? a1) (number? a2)) (+ a1 a2))
+          (else (list '+ a1 a2))))
+
+;化简乘式
+(define (make-product m1 m2)
+    (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+          ((=number? m1 1) m2)
+          ((=number? m2 1) m1)
+          ((and (number? m1) (number? m2)) (* m1 m2))
+          (else (list '* m1 m2))))
