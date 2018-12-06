5daee7a6a0a182b34f4838d882b6e65dbca4a48d
Perry961002 <Perry961002@163.com>
2018-12-06 17:46:07 +0800

    :tada:例2.3.3集合作为排序的表

--- Chap2/example/exa2.3.3-set.scm
+++ Chap2/example/exa2.3.3-set.scm
@@ -19,3 +19,25 @@
            (cons (car set1)
                  (intersection-set (cdr set1) set2)))
           (else (intersection-set (cdr set1) set2))))
+;-------------------------------------------------------------------
+;集合作为排序的表
+
+;对于有序集合的element-of-set?操作
+(define (element-of-set? x set)
+    (cond ((null? set) #f)
+          ((= x (car set)) #t)
+          ((< x (car set)) #f)
+          (else (element-of-set? x (cdr set)))))
+
+;对于有序集合的intersection-set操作
+(define (intersection-set set1 set2)
+    (if (or (null? set1) (null? set2))
+        '()
+        (let ((x1 (car set1)) (x2 (car set2)))
+            (cond ((= x1 x2)
+                   (cons x1
+                         (intersection-set (cdr set1) (cdr set2))))
+                  ((< x1 x2)
+                   (intersection-set (cdr set1) set2))
+                  ((< x2 x1)
+                   (intersection-set set1 (cdr set2)))))))
