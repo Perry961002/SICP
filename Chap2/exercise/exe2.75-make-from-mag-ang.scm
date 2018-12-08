ee711003b412debf84e28e122771d6386d5bb119
Perry961002 <Perry961002@163.com>
2018-12-08 21:58:33 +0800

    :art:习题2.75 消息传递机制的make-from-mag-ang过程

---
+++ Chap2/exercise/exe2.75-make-from-mag-ang.scm
@@ -0,0 +1,10 @@
+;使用消息传递重写make-from-mag-ang过程
+(define (make-from-mag-ang r a)
+    (define (dispatch op)
+        (cond ((eq? op 'magnitude) r)
+              ((eq? op 'angle) a)
+              ((eq? op 'real-part) (* r (cos a)))
+              ((eq? op 'imag-part) (* r (sin a)))
+              (else
+                (error "Unknown op -- MAKE-FROM-MAG-ANG" op))))
+    dispatch)
