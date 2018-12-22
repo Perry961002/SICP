5bae730bf938384db1d8c6633c394a1d04bbfbd8
Perry961002 <Perry961002@163.com>
2018-12-22 19:51:49 +0800

    :tada:习题3.28 或门

---
+++ Chap3/exercise/exe3.28-or-gate.scm
@@ -0,0 +1,17 @@
+;逻辑或
+(define (logical-or x y)
+    (if (and (= x 0) (= y 0))
+        0
+        1))
+
+;或门
+(define (or-gate a1 a2 output)
+    (define (or-action-procedure)
+        (let ((new-value
+                (logical-or (get-signal a1) (get-signal a2))))
+            (after-delay or-gate-delay
+                         (lambda ()
+                            (set-signal! output new-value)))))
+    (add-action! a1 or-action-procedure)
+    (add-action! a2 or-action-procedure)
+    'ok)
