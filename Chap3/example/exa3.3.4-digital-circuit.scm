6a376e4673cda52856fe6f02a992e050fc8ae675
Perry961002 <Perry961002@163.com>
2018-12-24 21:04:57 +0800

    ----

--- Chap3/example/exa3.3.4-digital-circuit.scm
+++ Chap3/example/exa3.3.4-digital-circuit.scm
@@ -81,9 +81,69 @@
 
 (define (add-action! wire action-procedure)
     ((wire 'add-action!) action-procedure))
+;------------------------------------------------------------------------
 
 ;待处理表
 (define (after-delay delay action)
     (add-to-agenda! (+ delay (current-time the-agenda))
                     action
                     the-agenda))
+
+;用propagate驱动模拟
+(define (propagate)
+    (if (empty-agenda? the-agenda)
+        'done
+        (let ((first-item (first-agenda-item the-agenda)))
+            (first-item)
+            (remove-first-agenda-item! the-agenda)
+            (propagate))))
+
+;------------------------------------------------------------------------
+;一个简单的实例模拟器
+
+;监测器
+(define (probe name wire)
+    (add-action! wire
+                 (lambda ()
+                    (newline)
+                    (display name)
+                    (display " ")
+                    (display (current-time the-agenda))
+                    (display "  New-value = ")
+                    (display (get-signal wire)))))
+
+;初始化处理表和描述各种功能块的延时
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-dely 3)
+(define or-gate 5)
+
+;定义4条线路，并安装监测器
+(define input-1 (make-wire))
+(define input-2 (make-wire))
+(define sum (make-wire))
+(define carry (make-wire))
+
+(probe 'sum sum)
+(probe 'carry carry)
+
+;将线路连接到半加器上
+(half-adder input-1 input-2 sum carry)
+
+(set-signal! input-1 1)
+
+(propagate)
+
+(set-signal! input-2 1)
+
+(propagate)
+;------------------------------------------------------------------
+;待处理表的实现\
+
+;这种待处理表由一些时间段组成，每个时间段是由一个数值和一个队列组成的序对
+(define (make-time-segment time queue)
+    (cons time queue))
+
+(define (segment-time s) (car s))
+
+(define (segment-queue s) (cdr s))
