51569c3fe3b34408aa1cde992b6eb69151fce7ab
Perry961002 <Perry961002@163.com>
2018-12-29 22:37:54 +0800

    :art:习题3.67 完全序对

--- Chap3/exercise/exe3.67-pairs.scm
+++ Chap3/exercise/exe3.67-pairs.scm
@@ -1,14 +1,14 @@
 ;混合进一个流，即(S1, T0), (S2, T0), ...
 (define (pairs s t)
     (stream-cons 
-        (list (stream-car s) (stream-cdr t))
+        (list (stream-car s) (stream-car t))
         (interleave
             (stream-map (lambda (x) (list (stream-car s) x))
                         (stream-cdr t))
             (interleave
                 (stream-map (lambda (x) (list x (stream-car t)))
                             (stream-cdr s))
-                (pairs (stream-cdr s) (stream-car t))))))
+                (pairs (stream-cdr s) (stream-cdr t))))))
 
 (display-top10 (pairs integers integers))
 ;(1 1)  (1 2)  (2 1)  (1 3)  (2 2)  (1 4)  (3 1)  (1 5)  (2 3)
