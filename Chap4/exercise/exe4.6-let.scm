1a832f505cc72419446d4f693fb66036342c1008
Perry961002 <Perry961002@163.com>
2019-01-03 23:00:03 +0800

    :art:习题4.6 let过程的实现

---
+++ Chap4/exercise/exe4.6-let.scm
@@ -0,0 +1,37 @@
+(define (let? exp) (tagged-list? exp 'let))
+
+(define (let-vars exp)
+    (map car (cadr exp)))
+
+(define (let-exps exp)
+    (map cadr (cadr exp)))
+
+(define (let-body exp)
+    (cddr exp))
+
+(define (let-combination exp)
+    (cons (make-lambda (let-vars exp) (let-body exp))
+          (let-exps exp)))
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp)
+          (make-procedure (lambda-parameters exp)
+                          (lambda-body exp)
+                          env))
+        ((begin? exp)
+          (eval-sequence (begin-actions exp) env))
+        ((cond? exp)
+          (eval (cond->if exp) env))
+        ((let? exp)
+          (eval (let->combination exp) env))
+        ((application? exp)
+          (apply (eval (operator exp) env)
+                 (list-of-values (operands exp) env)))
+        (else
+          (error "Unknown expression type -- EVAL " env))))
