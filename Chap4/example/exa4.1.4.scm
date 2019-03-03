4f72b758161bbd36b5e53732672d93488e1bdc3f
Perry961002 <Perry961002@163.com>
2019-03-04 05:17:39 +0800

    :tada:4.1.4 作为程序运行求值器

---
+++ Chap4/example/exa4.1.4.scm
@@ -0,0 +1,33 @@
+;安装环境
+(define (setup-environment)
+    (let ((initial-env
+            (extend-environment (primitive-procedure-names)
+                                (primitive-procedure-objects)
+                                the-empty-environment)))
+        (define-variable! 'true #t initial-env)
+        (define-variable! 'false #f initial-env)
+        initial-env))
+
+(define the-global-environment (setup-environment))
+
+(define (primitive-procedure? proc)
+    (tagged-list? proc 'primitive))
+
+
+(define (primitive-implementation proc) (cadr proc))
+
+(define primitive-procedures 
+    (list (list 'car car)
+          (list 'cdr cdr)
+          (list 'cons cons)
+          (list 'null? null?)))
+
+(define (primitive-procedure-names)
+    (map car primitive-procedures))
+
+(define (primitive-procedure-objects)
+    (map (lambda (proc) (list 'primitive (cadr proc)))
+         primitive-procedures))
+
+(define (apply-primitive-procedure proc args)
+    (apply (primitive-implementation proc) args))
