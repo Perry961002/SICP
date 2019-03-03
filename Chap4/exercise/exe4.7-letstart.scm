015b3df5e54b643a4d6135608efb4646fb99ee2b
Perry961002 <Perry961002@163.com>
2019-03-03 23:53:35 +0800

    let*过程

---
+++ Chap4/exercise/exe4.7-letstart.scm
@@ -0,0 +1,16 @@
+(load "Chap4\exercise\exe4.6-let.scm")
+
+;判断函数
+(define (let*? exp)
+    (taggesd-list exp 'let*))
+
+;利用let进行派生
+(define (expand-let bindings body)
+    (if (null? bindings)
+        body
+        (make-let (list (car bindings))
+                  (expand-let (cdr bindings) body))))
+
+(define (let*->nested-lets exp)
+    (expand-let (let-bindings exp)
+                (let-body exp)))
